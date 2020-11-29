7ba1dcb6fc7a95154dcc7390eea2056f680d1456
JonathanAckerman <jonathan@pragmapack.com>
2020-11-28 21:52:46 -0800

    Use mutable iter to init labels

--- main.cpp
+++ main.cpp
@@ -12,13 +12,15 @@ int main()
     
     // @Note (jonathan): I'm getting decent performance here which is a bit surprising.
     //  My expectation is sizeof(float) * 10 * 60000 = 2,400,000 bytes
-    std::vector<std::vector<float>> Y(trainingSize, std::vector(10, 0.0f));
+    std::vector<std::vector<float>> Y(trainingSize, std::vector<float>(10, 0.0f));
+    auto labelIter = trainingLabels;
     for (int i = 0; i < trainingSize; ++i)
     {
-        int label = (int)*trainingLabels;
+        int label = (int)*labelIter;
         if (label == 0) label = 10;
         Y[i][label - 1] = 1.0f;
-    }    
+        ++labelIter;
+    }
     
     //std::byte *testLabels = LoadLabels("t10k-labels.idx1-ubyte");
     //std::byte *testImages = LoadImages("t10k-images.idx3-ubyte");
