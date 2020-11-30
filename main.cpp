117828291f5aa343639c661872db9fb8a509da99
JonathanAckerman <jonathan@pragmapack.com>
2020-11-29 22:18:40 -0800

    Initialize weights, Clean up

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <cmath>        // exp
 
 #include "loader.cpp"
+#include "prng.cpp"
 
 using byte = std::byte;
 template <typename T>
@@ -25,36 +26,47 @@ int main(int argc, char** argv)
 {
     int s2 = std::atoi(argv[1]);
     int trainingSize = 60000;
+    int imageSize = 784;
     
-    // @Note (jonathan): byte 0..255 -> float 0.0f..1.0f for pixel darkness
     const byte *const trainingLabels = LoadLabels("train-labels.idx1-ubyte");
     const byte *const trainingImages = LoadImages("train-images.idx3-ubyte");
-
-    // @Note (jonathan): I'm getting decent performance here which is a bit surprising.
-    //  My expectation is sizeof(float) * 10 * 60000 = 2,400,000 bytes
-    vector<vector<float>> Y(trainingSize, vector<float>(10, 0.0f));
-    auto labelIter = trainingLabels;
-    for (int i = 0; i < trainingSize; ++i)
-    {
-        int label = (int)*labelIter;
-        if (label == 0) label = 10;
-        Y[i][label - 1] = 1.0f;
-        ++labelIter;
-    }
     
-    // @Note (jonathan): Images are 1D vectors of pixels
-    vector<vector<float>> inputLayer(trainingSize + 1, vector<float>(28*28, 1.0f));
-    auto imageIter = trainingImages;
-    for (int image = 0; image < trainingSize; ++image)
+    vector<vector<float>> Y(trainingSize, vector<float>(10, 0.0f));
     {
-        for (int pixel; pixel < 28*28; ++pixel)
+        auto labelIter = trainingLabels;
+        for (int i = 0; i < trainingSize; ++i)
         {
-            // @Note (jonathan): Skip the bias term
-            inputLayer[image + 1][pixel] = (float)imageIter / 255.0f;
+            int label = (int)*labelIter;
+            if (label == 0) label = 10;
+            Y[i][label - 1] = 1.0f;
+            ++labelIter;
         }
     }
     
-    vector<vector<float>> hiddenLayer(trainingSize + 1, vector<float>(s2*s2));
+    vector<vector<float>> inputLayer(trainingSize + 1, vector<float>(imageSize, 1.0f));
+    {
+        auto imageIter = trainingImages;
+        for (int image = 0; image < trainingSize; ++image)
+        {
+            for (int pixel; pixel < imageSize; ++pixel)
+            {
+                inputLayer[image + 1][pixel] = (float)*imageIter / 255.0f;
+            }
+        }
+    }
+    
+    vector<vector<float>> W1(trainingSize, vector<float>(trainingSize + 1));
+    {
+        for (auto& i : W1)
+        {
+            for (auto& j : i)
+            {
+                j = prng();
+            }
+        }
+    }
+    
+    vector<vector<float>> hiddenLayer(trainingSize + 1, vector<float>(s2));
     
     vector<float> outputLayer(10, 0.0f);
     
